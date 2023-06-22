fc273fa17cbd4528c86b5d63f1a547b51a702257
Saketh <sakethsai250@gmail.com>
2023-06-22 17:10:08 +0530

    day2 4th and day3 1st

--- module1/day2/4.c
+++ module1/day2/4.c
@@ -9,3 +9,33 @@ Topics to be covered
 - Loops
 - Bitwise Operators
 */
+
+#include <stdio.h>
+
+int countSetBits(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        if (num & 1)
+            count++;
+        num >>= 1;
+    }
+    return count;
+}
+
+int main()
+{
+    int a[] = {0x1, 0xF4, 0x10001};
+    int size = sizeof(a) / sizeof(a[0]);
+    int totalBits = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        totalBits += countSetBits(a[i]);
+    }
+
+    printf("The total number of set bits in the given array is %d\n", totalBits);
+
+    return 0;
+}
--- module1/day3/1.c
+++ module1/day3/1.c
@@ -9,3 +9,43 @@ Write a program to print the given pattern below using 3 functions (1st to accep
 Concepts to be used.
 - Loops
 */
+
+#include <stdio.h>
+
+void printNumbers(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d", i);
+    }
+}
+
+void printSpaces(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf(" ");
+    }
+}
+
+void printPattern(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        printNumbers(i);
+        printSpaces(2 * (n - i));
+        printNumbers(i);
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printPattern(n);
+
+    return 0;
+}
